3210b73b6a782c10036f5712c2fc82cee5a6aee6
Tommy White <tommywhite525@gmail.com>
2020-04-17 11:41:19 -0400

    Major clean up. Removed a lot of unnecessary code and comments.

--- src/main/java/pkgMain/ImgController2.java
+++ src/main/java/pkgMain/ImgController2.java
@@ -1,8 +1,6 @@
 package pkgMain;
 
 import javafx.event.EventHandler;
-import javafx.scene.Node;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class ImgController2 {
@@ -19,11 +17,6 @@ public class ImgController2 {
 	}
 	
 	public void drag(MouseEvent event) {
-		System.out.println("here2");
-//		Node n = (Node)event.getSource();
-//		if (DEBUG) System.out.println("ic mouse drag tx: " + n.getTranslateX() + ", ex: " + event.getX() );
-		if (DEBUG) System.out.println("ic mouse drag tx: " + model.getMouseXSinceDrag() + ", ex: " + event.getX() );
-//		model.setMouseYSinceDrag(model.getMouseYSinceDrag() + event.getY());
 		if (event.getX() > 100 && !copied) {
 			int index = view.addIVToFlow();
 			model.addX(0);
@@ -37,15 +30,11 @@ public class ImgController2 {
 	}
 	
 	public void dragFlow(int i, MouseEvent event) {
-		System.out.println("here3");
-//		Node n = (Node)event.getSource();
 		if (DEBUG) System.out.println("ic mouse drag tx: " + model.getXs(i) + ", ex: " + event.getX() );
 		model.setXs(i, model.getXs(i) + event.getX()); //event.getX() is the amount of horiz drag
 		model.setYs(i, model.getYs(i) + event.getY());
 		view.setXs(i, model.getXs(i) );
 		view.setYs(i, model.getYs(i) );
-		//n.setTranslateX(n.getTranslateX() + event.getX()); //not MVC! what problem does this create?
-		//n.setTranslateY(n.getTranslateY() + event.getY());
 	}
 	
 	public void press(MouseEvent event) {
@@ -56,7 +45,6 @@ public class ImgController2 {
 	public void dragReleased(MouseEvent event) {
 		System.out.println("released");
 		copied = true;
-		model.setMouseXSinceDrag(0);
 	}
 
 	public EventHandler getHandlerForDrag() {
@@ -74,27 +62,5 @@ public class ImgController2 {
 	public EventHandler getHandlerForPress() {
 		return event -> press((MouseEvent) event);
 	}
-	
-//	public EventHandler getHandlerForDragOver() {
-//		return event -> dragOver((MouseEvent) event);
-//	}
-	
-//	public EventHandler getHandlerForTileDrag() {
-//		return event -> copy((MouseEvent) event);
-//	}
-
-/*	public void tellModelStartingCoords(double x, double y) {
-		model.setX(view.getImgStartingX());
-		model.setY(view.getImgStartingY());
-		if (DEBUG) System.out.println("start: " + model.getX() + " " + model.getY());
-	}
-*/
-	public double getStartingX() {
-		return model.getX();
-	}
-	public double getStartingY() {
-		return model.getY();
-	}
-	
 	
 }
--- src/main/java/pkgMain/MyMovingImageModel2.java
+++ src/main/java/pkgMain/MyMovingImageModel2.java
@@ -4,27 +4,11 @@ import java.util.ArrayList;
 
 public class MyMovingImageModel2 {
 
-	private double x1 = 0;
-	private double y1 = 0;
-	private double mouseXSinceDrag = 0;
-	private double mouseYSinceDrag = 0;
 	private ArrayList<Double> xs = new ArrayList<Double>();
 	private ArrayList<Double> ys = new ArrayList<Double>();
 	private final double SIDE = 800;
 	private final double BOTTOM = 600;
 	
-	public double getX() {
-		return x1;
-	}
-	public void setX(double x) {
-		this.x1 = Math.min(x, SIDE);
-	}
-	public double getY() {
-		return y1;
-	}
-	public void setY(double y) {
-		this.y1 = Math.min(y, BOTTOM);
-	}
 	public double getXs(int index) {
 		return xs.get(index);
 	}
@@ -36,18 +20,6 @@ public class MyMovingImageModel2 {
 	}
 	public void setYs(int index, double y) {
 		ys.set(index, Math.min(y, BOTTOM));
-	}
-	public double getMouseXSinceDrag() {
-		return mouseXSinceDrag;
-	}
-	public double getMouseYSinceDrag() {
-		return mouseYSinceDrag;
-	}
-	public void setMouseXSinceDrag(double x) {
-		mouseXSinceDrag = x;
-	}
-	public void setMouseYSinceDrag(double y) {
-		mouseXSinceDrag = y;
 	}
 	public void addX(double x) {
 		xs.add(x);
